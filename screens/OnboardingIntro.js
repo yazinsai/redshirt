9d6e2e9dda284780d0efd959902cfe657985343f
Oscar Rodriguez <oscar@verify.as>
2019-01-08 11:40:38 +0100

    onboarding intro screen

---
+++ screens/OnboardingIntro.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+
+export default class Onboarding extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>With Red Shirt, you’ll <Text style={styles.bold}>never go to another laundry</Text> again.</Text>
+        <View style={styles.buttonsContainer}>
+          <TouchableOpacity style={[styles.button, styles.btnPrimary]} >
+            <Text style={[styles.btnText, styles.textPrimary]}>3 simple steps</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.btnSecondary]} >
+            <Text style={[styles.btnText, styles.textSecondary]}>Skip Intro</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: 100,
+    width: '90%',
+    marginLeft: '5%'
+  },
+  title: {
+    fontSize: 44,
+    color: '#D0021B',
+    fontFamily: 'Helvetica',
+    fontWeight: '100'
+  },
+  bold: {
+    fontWeight: "bold"
+  },
+  buttonsContainer: {
+    marginTop: 100
+  },
+  button: {
+    borderRadius: 25,
+    padding: 15,
+    margin: 10
+  },
+  btnPrimary: {
+    backgroundColor: '#D0021B'
+  },
+  btnSecondary: {
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: 'black',
+  },
+  btnText: {
+    textAlign: 'center',
+    fontSize: 14
+  },
+  textPrimary: {
+    color: 'white'
+  },
+});
