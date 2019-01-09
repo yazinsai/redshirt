ec22357bcf4fe2bc46278042811e3cec243a1034
Oscar Rodriguez <oscar@verify.as>
2019-01-09 18:36:03 +0100

    Laundry screen

--- screens/SelectLaundry.js
+++ screens/SelectLaundry.js
@@ -1,21 +1,22 @@
 import React, { Component } from "react";
-import { Text, FlatList } from "react-native";
+import {  FlatList } from "react-native";
+import { Card } from 'react-native-elements'
 
 import laundries from "../data/laundries";
-import { LaundryItem } from "../components/List";
-import { Container } from "../components/Container";
+import LaundryItem from "../components/LaundryItem";
 
 class ChooseLaundry extends Component {
+  static navigationOptions = {
+    headerMode: 'float',
+  }
   render() {
     return (
-      <Container>
-        <Text>Select your Laundry from the below:</Text>
         <FlatList
           data={laundries}
           renderItem={({ item }) => <LaundryItem item={item} />}
           keyExtractor={item => item.id}
         />
-      </Container>
+      
     );
   }
 }
