f9d7958d81ac86d6ccc506245c4259fd3c8f7062
Yazin <yazin@verify.as>
2019-01-12 18:44:39 +0300

    Update container to flex: 1

--- screens/OnboardingIntro.js
+++ screens/OnboardingIntro.js
@@ -1,29 +1,26 @@
 import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { StyleSheet, StatusBar, Text, View, TouchableOpacity } from "react-native";
 import Button from '../components/Button'
+import { Container } from '../components/Container'
 
 export default class OnboardingIntro extends React.Component {
   render() {
     const {navigate} = this.props.navigation;
     return (
-      <View style={styles.container}>
+      <Container style={styles.container}>
+        <StatusBar barStyle="dark-content" />
         <Text style={styles.title}>With Red Shirt, you’ll <Text style={styles.bold}>never go to another laundry</Text> again.</Text>
         <View style={styles.buttonsContainer}>
           <Button variant='primary' onPress={() => navigate('OnBoardingSlides')} text='3 simple steps' />
           <Button variant='secondary' onPress={() => navigate('Home')} text='Skip intro' />
         </View>
-      </View>
+      </Container>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    paddingTop: 100,
-    width: '90%',
-    marginLeft: '5%'
   },
   title: {
     fontSize: 44,
