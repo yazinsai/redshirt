92de31e136a3b95f2655107e2fae645acb3d4682
Oscar Rodriguez <oscar@verify.as>
2019-01-16 08:56:18 +0100

    change pickup based on time, selectors now show day of the week and formatted date sent to the server

--- screens/OrderDetails.js
+++ screens/OrderDetails.js
@@ -17,16 +17,19 @@ import Button from '../components/Button'
 import { Formik } from "formik";
 
 const Form = t.form.Form;
-const DATE_FORMAT = "YYYY-MM-DD";
+const DATE_FORMAT_FULL = "YYYY-MM-DD";
+const DATE_FORMAT_DAY = 'dddd'
 
 function get2DaysOption() {
   const twoDays = moment(new Date())
-      .add(2, "days")
-      .format(DATE_FORMAT);
+    .add(2, "days")
+  const twoDaysFull = twoDays.format(DATE_FORMAT_FULL)
+  const twoDaysWeek = twoDays.format(DATE_FORMAT_DAY)
+
   const result = {} 
-  result[`${twoDays}/M`] = `${twoDays}, morning (9am-12pm)`
-  result[`${twoDays}/A`] = `${twoDays}, afernoon (12pm-4pm)`
-  result[`${twoDays}/E`] = `${twoDays}, evening (4pm-8pm)`
+  result[`${twoDaysFull}/M`] = `${twoDaysWeek}, morning (9am-12pm)`
+  result[`${twoDaysFull}/A`] = `${twoDaysWeek}, afernoon (12pm-4pm)`
+  result[`${twoDaysFull}/E`] = `${twoDaysWeek}, evening (4pm-8pm)`
   return result
 }
 
@@ -55,28 +58,12 @@ class OrderDetails extends  React.Component {
     super(props);
 
     this.state = {
-      soonestDropOffDate: moment().format(DATE_FORMAT),
-      soonestDropOffTime: 0,
       type: this.getType({}),
       value: {}
     };
 
-    this.onChangePickup = this.onChangePickup.bind(this);
     this.submitForm = this.submitForm.bind(this)
     this.onChange = this.onChange.bind(this)
-  }
-
-
-
-  onChangePickup({ date, time }) {
-    const tomorrow = moment(date)
-      .add(1, "days")
-      .format(DATE_FORMAT);
-
-    this.setState({
-      soonestDropOffDate: tomorrow,
-      soonestDropOffTime: time
-    });
   }
 
   onChange(value) {
@@ -98,12 +85,17 @@ class OrderDetails extends  React.Component {
     const result = {}
     if(pickup) {
       const [pickUpDay, pickUpTime] = pickup.split('/')
+
       const twoDays = moment(new Date())
         .add(2, "days")
-        .format(DATE_FORMAT);
+      const twoDaysFull = twoDays.format(DATE_FORMAT_FULL)
+      const twoDaysWeek = twoDays.format(DATE_FORMAT_DAY)
+
       const threeDays = moment(new Date())
-      .add(3, "days")
-      .format(DATE_FORMAT);
+        .add(3, "days")
+      const threeDaysFull = threeDays.format(DATE_FORMAT_FULL)
+      const threeDaysWeek = threeDays.format(DATE_FORMAT_DAY)
+      
       if(pickUpDay == 'TD') {
         if(pickUpTime == 'M') {
           result['TM/M'] = 'Tomorrow, morning(9am-12pm)'
@@ -115,71 +107,77 @@ class OrderDetails extends  React.Component {
         } else if(pickUpTime == 'E') {
           result['TM/E'] = 'Tomorrow, evening (4pm-8pm)'
         }
-        result[`${twoDays}/M`] = `${twoDays}, morning (9am-12pm)`
-        result[`${twoDays}/A`] = `${twoDays}, afernoon (12pm-4pm)`
-        result[`${twoDays}/E`] = `${twoDays}, evening (4pm-8pm)`
-        result[`${threeDays}/M`] = `${threeDays}, morning (9am-12pm)`
-        result[`${threeDays}/A`] = `${threeDays}, afernoon (12pm-4pm)`
-        result[`${threeDays}/E`] = `${threeDays}, evening (4pm-8pm)`
+        result[`${twoDaysFull}/M`] = `${twoDaysWeek}, morning (9am-12pm)`
+        result[`${twoDaysFull}/A`] = `${twoDaysWeek}, afernoon (12pm-4pm)`
+        result[`${twoDaysFull}/E`] = `${twoDaysWeek}, evening (4pm-8pm)`
+        result[`${threeDaysFull}/M`] = `${threeDaysWeek}, morning (9am-12pm)`
+        result[`${threeDaysFull}/A`] = `${threeDaysWeek}, afernoon (12pm-4pm)`
+        result[`${threeDaysFull}/E`] = `${threeDaysWeek}, evening (4pm-8pm)`
       } else if(pickUpDay == 'TM') {
         const fourDays = moment(new Date())
-        .add(4, "days")
-        .format(DATE_FORMAT);
+          .add(4, "days")
+        const fourDaysFull = fourDays.format(DATE_FORMAT_FULL)
+        const fourDaysWeek = fourDays.format(DATE_FORMAT_DAY)
+
         if(pickUpTime == 'M') {
-          result[`${twoDays}/M`] = `${twoDays}, morning (9am-12pm)`
-          result[`${twoDays}/A`] = `${twoDays}, afernoon (12pm-4pm)`
-          result[`${twoDays}/E`] = `${twoDays}, evening (4pm-8pm)`
+          result[`${twoDaysFull}/M`] = `${twoDaysWeek}, morning (9am-12pm)`
+          result[`${twoDaysFull}/A`] = `${twoDaysWeek}, afernoon (12pm-4pm)`
+          result[`${twoDaysFull}/E`] = `${twoDaysWeek}, evening (4pm-8pm)`
         } else if(pickUpTime == 'A') {
-          result[`${twoDays}/A`] = `${twoDays}, afernoon (12pm-4pm)`
-          result[`${twoDays}/E`] = `${twoDays}, evening (4pm-8pm)`
+          result[`${twoDaysFull}/A`] = `${twoDaysWeek}, afernoon (12pm-4pm)`
+          result[`${twoDaysFull}/E`] = `${twoDaysWeek}, evening (4pm-8pm)`
         } else if(pickUpTime == 'E') {
-          result[`${twoDays}/E`] = `${twoDays}, evening (4pm-8pm)`
+          result[`${twoDaysFull}/E`] = `${twoDaysWeek}, evening (4pm-8pm)`
         }
-        result[`${threeDays}/M`] = `${threeDays}, morning (9am-12pm)`
-        result[`${threeDays}/A`] = `${threeDays}, afernoon (12pm-4pm)`
-        result[`${threeDays}/E`] = `${threeDays}, evening (4pm-8pm)`
-        result[`${fourDays}/M`] = `${fourDays}, morning (9am-12pm)`
-        result[`${fourDays}/A`] = `${fourDays}, afernoon (12pm-4pm)`
-        result[`${fourDays}/E`] = `${fourDays}, evening (4pm-8pm)`
+        result[`${threeDaysFull}/M`] = `${threeDaysWeek}, morning (9am-12pm)`
+        result[`${threeDaysFull}/A`] = `${threeDaysWeek}, afernoon (12pm-4pm)`
+        result[`${threeDaysFull}/E`] = `${threeDaysWeek}, evening (4pm-8pm)`
+        result[`${fourDaysFull}/M`] = `${fourDaysWeek}, morning (9am-12pm)`
+        result[`${fourDaysFull}/A`] = `${fourDaysWeek}, afernoon (12pm-4pm)`
+        result[`${fourDaysFull}/E`] = `${fourDaysWeek}, evening (4pm-8pm)`
       } else {
         const oneDayFromDate = moment(pickUpDay)
           .add(1, "days")
           .format(DATE_FORMAT);
+        const oneDayFromDateFull = oneDayFromDate.format(DATE_FORMAT_FULL)
+        const oneDayFromDateWeek = oneDayFromDate.format(DATE_FORMAT_DAY)
+
         const twoDaysFromDate = moment(pickUpDay)
           .add(2, "days")
           .format(DATE_FORMAT);
+        const twoDaysFromDateFull = twoDaysFromDate.format(DATE_FORMAT_FULL)
+        const twoDaysFromDateWeek = twoDaysFromDate.format(DATE_FORMAT_DAY)
+
         const threeDaysFromDate = moment(pickUpDay)
           .add(3, "days")
           .format(DATE_FORMAT);
+        const threeDaysFromDateFull = threeDaysFromDate.format(DATE_FORMAT_FULL)
+        const threeDaysFromDateWeek = threeDaysFromDate.format(DATE_FORMAT_DAY)
+
         if(pickUpTime == 'M') {
-          result[`${oneDayFromDate}/M`] = `${oneDayFromDate}, morning (9am-12pm)`
-          result[`${oneDayFromDate}/A`] = `${oneDayFromDate}, afernoon (12pm-4pm)`
-          result[`${oneDayFromDate}/E`] = `${oneDayFromDate}, evening (4pm-8pm)`
+          result[`${oneDayFromDateFull}/M`] = `${oneDayFromDateWeek}, morning (9am-12pm)`
+          result[`${oneDayFromDateFull}/A`] = `${oneDayFromDateWeek}, afernoon (12pm-4pm)`
+          result[`${oneDayFromDateFull}/E`] = `${oneDayFromDateWeek}, evening (4pm-8pm)`
         } else if(pickUpTime == 'A') {
-          result[`${oneDayFromDate}/A`] = `${oneDayFromDate}, afernoon (12pm-4pm)`
-          result[`${oneDayFromDate}/E`] = `${oneDayFromDate}, evening (4pm-8pm)`
+          result[`${oneDayFromDateFull}/A`] = `${oneDayFromDateWeek}, afernoon (12pm-4pm)`
+          result[`${oneDayFromDateFull}/E`] = `${oneDayFromDateWeek}, evening (4pm-8pm)`
         } else if(pickUpTime == 'E') {
-          result[`${oneDayFromDate}/E`] = `${oneDayFromDate}, evening (4pm-8pm)`
+          result[`${oneDayFromDateFull}/E`] = `${oneDayFromDateWeek}, evening (4pm-8pm)`
         }
-        result[`${twoDaysFromDate}/M`] = `${twoDaysFromDate}, morning (9am-12pm)`
-        result[`${twoDaysFromDate}/A`] = `${twoDaysFromDate}, afernoon (12pm-4pm)`
-        result[`${twoDaysFromDate}/E`] = `${twoDaysFromDate}, evening (4pm-8pm)`
-        result[`${threeDaysFromDate}/M`] = `${threeDaysFromDate}, morning (9am-12pm)`
-        result[`${threeDaysFromDate}/A`] = `${threeDaysFromDate}, afernoon (12pm-4pm)`
-        result[`${threeDaysFromDate}/E`] = `${threeDaysFromDate}, evening (4pm-8pm)`
+
+        result[`${twoDaysFromDateFull}/M`] = `${twoDaysFromDateWeek}, morning (9am-12pm)`
+        result[`${twoDaysFromDateFull}/A`] = `${twoDaysFromDateWeek}, afernoon (12pm-4pm)`
+        result[`${twoDaysFromDateFull}/E`] = `${twoDaysFromDateWeek}, evening (4pm-8pm)`
+
+        result[`${threeDaysFromDateFull}/M`] = `${threeDaysFromDateWeek}, morning (9am-12pm)`
+        result[`${threeDaysFromDateFull}/A`] = `${threeDaysFromDateWeek}, afernoon (12pm-4pm)`
+        result[`${threeDaysFromDateFull}/E`] = `${threeDaysFromDateWeek}, evening (4pm-8pm)`
       }
     }
     const deliverType = t.enums(result);
+    const pickupType = this.getPickupBasedOnTime()
     const Order = t.struct({
-      pickup: t.enums({
-        'TD/M': 'Today, morning (9am-12pm)',
-        'TD/A': 'Today, afernoon (12pm-4pm)',
-        'TD/E': 'Today, evening (4pm-8pm)',
-        'TM/M': 'Tomorrow, morning (9am-12pm)',
-        'TM/A': 'Tomorrow, afernoon (12pm-4pm)',
-        'TM/E': 'Tomorrow, evening (4pm-8pm)',
-        ...get2DaysOption()
-      }, 'pickUp'),
+      pickup: pickupType,
       deliver: deliverType,
       address: t.String,
       phone: t.Number,
@@ -188,12 +186,33 @@ class OrderDetails extends  React.Component {
     return Order
   }
 
+  getPickupBasedOnTime() {
+    const hour = new Date().getHours()
+    const result = {}
+    if(hour<19) {
+      if(hour<15){
+        if(hour<11) {
+          result['TD/M'] = 'Today, morning (9am-12pm)'
+        }
+        result['TD/A'] = 'Today, afternoon (12pm-4pm)'
+      }
+      result['TD/E'] = 'Today, evening (4pm-8pm)'
+    }
+    result['TM/M'] = 'Tomorrow, morning (9am-12pm)'
+    result['TM/A'] = 'Tomorrow, afternoon (12pm-4pm)'
+    result['TM/E'] = 'Tomorrow, evening (4pm-8pm)'
+    return t.enums({...result, ...get2DaysOption()})
+  }
+
   submitForm() {
     const { navigation } = this.props;
     const formValue = this.refs.form.getValue()
     if(!formValue) return
     const laundry = navigation.getParam('laundry', 'NO-LAUNDRY');
-    const result = Object.assign({}, formValue, {laundry})
+    const pickup = this.formatDate(formValue.pickup)
+    const deliver = this.formatDate(formValue.deliver)
+    const result = Object.assign({}, formValue, {laundry, pickup, deliver})
+    
     const json = JSON.stringify(result)
     fetch('https://shine-server-order.herokuapp.com', {
     method: 'POST',
@@ -205,6 +224,34 @@ class OrderDetails extends  React.Component {
   }).then(()=> navigation.navigate('Feedback'))
   }
 
+  formatDate(date){
+    const [dateDay, dateTime] = date.split('/')
+    let formattedDay = ''
+    if(dateDay == 'TD') {
+      const today = moment(new Date())
+        .format(DATE_FORMAT_FULL);
+      formattedDay = today
+    } else if (dateDay == 'TM') {
+      const tomorrow = moment(new Date())
+        .add(1, 'days')
+        .format(DATE_FORMAT_FULL);
+      formattedDay = tomorrow
+    } else {
+      formattedDay = dateDay
+    }
+
+    let formattedTime = ''
+
+    if(dateTime == 'M') {
+      formattedTime = 'before 12pm'
+    } else if(dateTime == 'A') {
+      formattedTime = 'before 4pm'
+    } else {
+      formattedTime = 'before 8pm'
+    }
+
+    return `${formattedDay} ${formattedTime}`
+  }
   
   render() {
     const { navigate } = this.props.navigation;
