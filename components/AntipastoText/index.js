09276ae3124e0fd1959e9ea50c78f86b5bbe7ffb
Oscar Rodriguez <oscar@verify.as>
2019-01-16 13:29:52 +0100

    remade text component with new utility

--- components/AntipastoText/index.js
+++ components/AntipastoText/index.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react'
 import {Text} from 'react-native'
 
+import fontMaker from '../../util/fontMaker'
+
 export class AntipastoText extends Component {
   render() {
+    let weight = 'Regular'
+    if(this.props.weight){
+      weight = this.props.weight
+    }
     return (
-      <Text style={[this.props.style,{fontWeight: "600", fontFamily: 'AntipastoPro'}]}>{this.props.children}</Text>
+      <Text style={[this.props.style, fontMaker({ family: 'Antipasto', weight })]}>
+        {this.props.children}
+      </Text>
     );
   }
 }
