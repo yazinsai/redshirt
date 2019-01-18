ec510999014c1abe0c9879da14fcd2011414a734
Oscar Rodriguez <oscar@verify.as>
2019-01-18 13:59:38 +0100

    copied strings for arab language

--- localization/strings.js
+++ localization/strings.js
@@ -47,4 +47,25 @@ export const ar = {
   onBoardingSecondSlideLight: ' your laundry in a bag',
   onBoardingThirdSlideBold: 'We return',
   onBoardingThirdSlideLight: ' your clean laundry',
+  onBoardingThirdSlideButton: 'Place order',
+
+  homeAppLogoText: 'Red Shirt',
+  homePickupButton: 'Schedule a pickup',
+  homeCollectAboveText: 'Already dropped off your laundry?',
+  homeCollectButton: 'Collect from the laundry',
+
+  orderDetailsMorning: 'morning (9am - 12pm)',
+  orderDetailsAfternoon: 'afternoon (12pm - 4pm)',
+  orderDetailsEvening: 'evening (4pm - 8pm)',
+  orderDetailsToday: 'Today,',
+  orderDetailsTomorrow: 'Tomorrow,',
+  orderDetailsPickup: 'When can we pick up your laundry?',
+  orderDetailsDeliver: 'When can we return your laundry?',
+  orderDetailsAddress: 'What is your address?',
+  orderDetailsPhone: 'Your mobile number',
+  orderDetailsEmail: 'Your email address (for your receipt)',
+  orderDetailsSubmitButton: 'Place my order',
+
+  feedbackTop: 'You\'re all \n  set.',
+  feedbackBottom: 'See you ',
 }
