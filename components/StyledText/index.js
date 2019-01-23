f9c87485df98441aea76b740f2b424d7924dc907
Oscar Rodriguez <oscar@verify.as>
2019-01-23 10:54:35 +0100

    added texts from h1 to h5

--- components/StyledText/index.js
+++ components/StyledText/index.js
@@ -18,6 +18,8 @@ export class StyledText extends Component {
     let localeStyle = {}
     let sizeClass = this.props.size ? this.props.size : 'body'
 
+   
+
     if(Localization.locale == 'ar'){
       localeStyle = {
         writingDirection: 'rtl'
@@ -26,6 +28,7 @@ export class StyledText extends Component {
       sizeClass += 'AR'
     }
 
+    console.log(sizeClass)
 
     return (
       <Text style={[this.props.style, fontMaker({ family, weight }), 
@@ -48,6 +51,36 @@ const styles = StyleSheet.create({
   },
   bodyAR: {
     fontSize: 16
+  },
+  h1: {
+    fontSize: 150
+  },
+  h1AR: {
+    fontSize: 140
+  },
+  h2: {
+    fontSize: 72
+  },
+  h2AR: {
+    fontSize: 60
+  },
+  h3: {
+    fontSize: 54
+  },
+  h3AR: {
+    fontSize: 48
+  },
+  h4: {
+    fontSize: 32
+  },
+  h4AR: {
+    fontSize: 28
+  },
+  h5: {
+    fontSize: 24
+  },
+  h5AR: {
+    fontSize: 21
   }
 })
 
